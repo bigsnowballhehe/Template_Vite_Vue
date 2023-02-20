c9230ce0660c35cad2da632564fdb490ae4c06ed
alan.wu <snfzi@qq.com>
2023-02-20 19:36:05 +0800

    feat:add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-
 <template>
-
+  <div />
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from "unocss/vite";
-import path from "path"
+import UnoCSS from 'unocss/vite'
 import presetUno from '@unocss/preset-uno'
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [vue(), UnoCSS({
-    presets: [presetUno()
-    ]
-  }),],
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-  // esbuild: {
-  //   drop: ['console', 'debugger']
-  // },
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => ({
+  base: './',
+  plugins: [vue({
+    reactivityTransform: true,
+  }), UnoCSS({
+    presets: [presetUno(),
+      AutoImport({
+        imports: ['vue'],
+        dts: true,
+        vueTemplate: true,
+        cache: true,
+      }),
+    ],
+  })],
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
   },
-})
+  server: {
+    port: 3000,
+  },
+}))
